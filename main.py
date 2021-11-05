102e4a8642b97f01ae1f0a22b3e26aa9374cf948
Ramunechoco <48040337+ramunechoco@users.noreply.github.com>
2021-11-05 10:03:01 +0700

    change dataset to training dataset

--- main.py
+++ main.py
@@ -6,29 +6,32 @@ from LEMBUT.model import Sequential
 def main():
     # Load dataset
     dataset_train = pd.read_csv('./dataset/bitcoin_price_Training - Training.csv')
-    dataset = pd.read_csv('./dataset/bitcoin_price_1week_Test - Test.csv')
+    # dataset = pd.read_csv('./dataset/bitcoin_price_1week_Test - Test.csv')
 
     # Preprocessing
-    dataset_train = dataset.drop(columns=['Date'])
+    dataset_train = dataset_train.iloc[0:32, :]
+    dataset_train = dataset_train.iloc[::-1].reset_index(drop=True)
+    dataset_train = dataset_train.drop(columns=['Date'])
     dataset_train['Volume'] = dataset_train['Volume'].apply(
         lambda x: x.replace(',', ''))
     dataset_train['Market Cap'] = dataset_train['Market Cap'].apply(
         lambda x: x.replace(',', ''))
     dataset_train['Volume'] = pd.to_numeric(dataset_train['Volume'])
     dataset_train['Market Cap'] = pd.to_numeric(dataset_train['Market Cap'])
+    # print(dataset_train)
 
-    dataset = dataset.drop(columns=['Date'])
-    dataset['Volume'] = dataset['Volume'].apply(
-        lambda x: x.replace(',', ''))
-    dataset['Market Cap'] = dataset['Market Cap'].apply(
-        lambda x: x.replace(',', ''))
-    dataset['Volume'] = pd.to_numeric(dataset['Volume'])
-    dataset['Market Cap'] = pd.to_numeric(dataset['Market Cap'])
+    # dataset = dataset.drop(columns=['Date'])
+    # dataset['Volume'] = dataset['Volume'].apply(
+    #     lambda x: x.replace(',', ''))
+    # dataset['Market Cap'] = dataset['Market Cap'].apply(
+    #     lambda x: x.replace(',', ''))
+    # dataset['Volume'] = pd.to_numeric(dataset['Volume'])
+    # dataset['Market Cap'] = pd.to_numeric(dataset['Market Cap'])
 
     # Create model
     model = Sequential()
     model.add(layers.LSTM(64, input_shape=(1, 4, 6), name='lstm_1'))
-    prediction = model.predict(dataset)
+    prediction = model.predict(dataset_train)
     print(prediction)
     model.summary()
 
